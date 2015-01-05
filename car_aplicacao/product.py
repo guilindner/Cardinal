01717effe4941955aed22883d579a90dd663060a
Guilherme Lindner <lindner.guilherme@gmail.com>
2015-01-05 15:06:38 -0200

    modulo aplicacao adicionado

---
+++ car_aplicacao/product.py
@@ -0,0 +1,9 @@
+class product_product(osv.osv):
+    _inherit = "product.product"
+
+    _columns = {
+                'car_aplicacao': fields.char('Aplicação', size=20),
+                                    help="Produto ou componente onde a peça pode ser utilizada."),
+    }
+
+product_product()
