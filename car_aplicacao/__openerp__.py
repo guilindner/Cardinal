214545694297fb2a417a58cea4f8e21352514390
Guilherme Lindner <lindner.guilherme@gmail.com>
2015-01-12 07:59:11 -0200

    ok

--- car_aplicacao/__openerp__.py
+++ car_aplicacao/__openerp__.py
@@ -4,7 +4,7 @@
 
     'summary': """
         Adiciona o campo Aplicacao na aba principal dos produtos
-        blablabla""",
+        """,
 
     'description': """
         Long description of module's purpose
@@ -12,19 +12,11 @@
 
     'author': "Cardinal",
     'website': "http://www.cardinal.com.br",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Product',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base','product'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-       # 'templates.xml',
+         'product_view.xml',
     ],
 }
--- car_aplicacao/product.py
+++ car_aplicacao/product.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from openerp.osv import osv, fields, expression
+
 class product_product(osv.osv):
-    _inherit = "product.product"
+    _name = "product.template"
+    _inherit = "product.template"
 
     _columns = {
-                'car_aplicacao': fields.char('Aplicação', size=20),
-                                    help="Produto ou componente onde a peça pode ser utilizada."),
+                'car_aplicacao': fields.char('Aplicação', size=20, help="Produto ou componente onde este item é utilizado"),
     }
 
 product_product()
